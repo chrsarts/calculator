fdd2a94bb64a1317fb769304c038289e838f4571
Chris Artates <ch1artates@gmail.com>
2022-06-22 19:23:28 +0800

    you can now subtract, multiply, and divide

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const numBtn = document.querySelectorAll(".num-button");
+
 const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+const multiplyBtn = document.getElementById("multiply");
+const divideBtn = document.getElementById("divide");
 const equalsBtn = document.getElementById("equals");
+
 let displayText = document.getElementById("display-text");
+
 let operand = "";
 let currNum = 0;
 let prevNum = 0;
@@ -28,10 +34,44 @@ plusBtn.addEventListener("click", () => {
   displayText.textContent = "0";
 });
 
-equalsBtn.addEventListener("click", () => {
-  if (operand === "plus") {
-    currNum = parseInt(displayText.textContent);
-    result = prevNum + currNum;
-    displayText.textContent = result;
+minusBtn.addEventListener("click", () => {
+  if (prevNum === 0) {
+    prevNum = parseInt(displayText.textContent);
+    console.log(prevNum);
   }
+  operand = "minus";
+  displayText.textContent = "0";
+});
+
+multiplyBtn.addEventListener("click", () => {
+  if (prevNum === 0) {
+    prevNum = parseInt(displayText.textContent);
+    console.log(prevNum);
+  }
+  operand = "multiply";
+  displayText.textContent = "0";
+});
+
+divideBtn.addEventListener("click", () => {
+  if (prevNum === 0) {
+    prevNum = parseInt(displayText.textContent);
+    console.log(prevNum);
+  }
+  operand = "divide";
+  displayText.textContent = "0";
+});
+
+equalsBtn.addEventListener("click", () => {
+  currNum = parseInt(displayText.textContent);
+  if (operand === "plus") {
+    result = prevNum + currNum;
+  } else if (operand === "minus") {
+    result = prevNum - currNum;
+  } else if (operand === "multiply") {
+    result = prevNum * currNum;
+  } else if (operand === "divide") {
+    result = prevNum / currNum;
+  }
+  displayText.textContent = result;
+  prevNum = result;
 });
