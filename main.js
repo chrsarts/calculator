9a057beb75cf1405a7d457645542ec8aacd580a6
Chris Artates <ch1artates@gmail.com>
2022-06-22 18:38:42 +0800

    numbers can now be added

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 const numBtn = document.querySelectorAll(".num-button");
+const plusBtn = document.getElementById("plus");
+const equalsBtn = document.getElementById("equals");
 let displayText = document.getElementById("display-text");
+let operand = "";
 let currNum = 0;
 let prevNum = 0;
+let result = 0;
 
 displayText.textContent = "0";
 
 numBtn.forEach((num) => {
   num.addEventListener("click", () => {
-    displayText.textContent += num.id;
+    if (displayText.textContent === "0" || displayText.textContent === 0) {
+      displayText.textContent = num.id;
+    } else {
+      displayText.textContent += num.id;
+    }
   });
 });
 
-function addition(currNum, prevNum) {
-  return currNum + prevNum;
-}
+plusBtn.addEventListener("click", () => {
+  if (prevNum === 0) {
+    prevNum = parseInt(displayText.textContent);
+    console.log(prevNum);
+  }
+  operand = "plus";
+  displayText.textContent = "0";
+});
 
-function subtraction(currNum, prevNum) {
-  return currNum - prevNum;
-}
-
-function multiplication(currNum, prevNum) {
-  return currNum * prevNum;
-}
-
-function division(currNum, prevNum) {
-  return currNum / prevNum;
-}
+equalsBtn.addEventListener("click", () => {
+  if (operand === "plus") {
+    currNum = parseInt(displayText.textContent);
+    result = prevNum + currNum;
+    displayText.textContent = result;
+  }
+});
