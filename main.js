4884135edaea845a5e0e6f7cf6d48d812b440511
Chris Artates <ch1artates@gmail.com>
2022-06-23 14:49:25 +0800

    added calculator styling(background color) and footer

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const numBtn = document.querySelectorAll(".num-button");
+const equalsBtn = document.getElementById("equals");
+const resetBtn = document.getElementById("clear");
 
 const plusBtn = document.getElementById("plus");
 const minusBtn = document.getElementById("minus");
 const multiplyBtn = document.getElementById("multiply");
 const divideBtn = document.getElementById("divide");
-const equalsBtn = document.getElementById("equals");
 
 let displayText = document.getElementById("display-text");
 
@@ -25,37 +26,64 @@ numBtn.forEach((num) => {
   });
 });
 
+resetBtn.addEventListener("click", () => {
+  operand = "";
+  currNum = 0;
+  prevNum = 0;
+  result = 0;
+  displayText.textContent = "0";
+});
+
 plusBtn.addEventListener("click", () => {
-  if (prevNum === 0) {
+  if (prevNum === 0 && currNum === 0) {
     prevNum = parseInt(displayText.textContent);
-    console.log(prevNum);
+  } else {
+    currNum = parseInt(displayText.textContent);
+    result = prevNum + currNum;
+    prevNum = result;
+    console.log(result);
   }
   operand = "plus";
   displayText.textContent = "0";
 });
 
 minusBtn.addEventListener("click", () => {
-  if (prevNum === 0) {
+  if (prevNum === 0 && currNum === 0) {
     prevNum = parseInt(displayText.textContent);
     console.log(prevNum);
+  } else {
+    currNum = parseInt(displayText.textContent);
+    result = prevNum - currNum;
+    prevNum = result;
+    console.log(result);
   }
   operand = "minus";
   displayText.textContent = "0";
 });
 
 multiplyBtn.addEventListener("click", () => {
-  if (prevNum === 0) {
+  if (prevNum === 0 && currNum === 0) {
     prevNum = parseInt(displayText.textContent);
     console.log(prevNum);
+  } else {
+    currNum = parseInt(displayText.textContent);
+    result = prevNum * currNum;
+    prevNum = result;
+    console.log(result);
   }
   operand = "multiply";
   displayText.textContent = "0";
 });
 
 divideBtn.addEventListener("click", () => {
-  if (prevNum === 0) {
+  if (prevNum === 0 && currNum === 0) {
     prevNum = parseInt(displayText.textContent);
     console.log(prevNum);
+  } else {
+    currNum = parseInt(displayText.textContent);
+    result = prevNum / currNum;
+    prevNum = result;
+    console.log(result);
   }
   operand = "divide";
   displayText.textContent = "0";
